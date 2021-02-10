973a8eb9813c4a68d8e3f06c729235b9fa904c42
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2021-02-10 10:34:52 +0300

    10.02.2021

--- src/diploma/home/urls.py
+++ src/diploma/home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('articles/', views.all_articles, name='all-articles'),
 ]
