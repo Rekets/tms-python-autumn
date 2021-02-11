9424a7ca284fa105ea3ac6ce59005a8ada173f84
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2021-02-11 19:06:07 +0300

    11.02.2021

--- src/diploma/user_profile/models.py
+++ src/diploma/user_profile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=30, blank=True)
+    height = models.CharField(max_length=3, blank=True)
+    weight = models.CharField(max_length=3, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    email = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.user.username
