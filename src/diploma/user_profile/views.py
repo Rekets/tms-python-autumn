19658ec49e3a3e31fc99d00e6c2d62342c53bcf3
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2021-02-11 18:15:51 +0300

    11.02.2021

---
+++ src/diploma/user_profile/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    return render(request, 'user/profile.html', {'user':user})
+
