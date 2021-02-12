779f68dea6dbb01583badd4e70331b1e46660b28
Rekets Alexander <73691955+Rekets@users.noreply.github.com>
2021-02-12 19:00:40 +0300

    12.02.2021

--- src/diploma/user_profile/urls.py
+++ src/diploma/user_profile/urls.py
@@ -4,4 +4,6 @@ from user_profile import views
 
 urlpatterns = [
     path('<str:username>/', views.profile, name='user-profile'),
+    path('<str:username>/edit/', views.edit_profile, name='edit-profile'),
+    path('<str:username>/delete/', views.delete_profile, name='delete-profile'),
 ]
--- src/diploma/user_profile/views.py
+++ src/diploma/user_profile/views.py
@@ -1,9 +1,36 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def profile(request, username):
-    user = User.objects.get(username=username)
-    return render(request, 'user/profile.html', {'user':user})
+    if request.user.is_authenticated:
+        user = get_object_or_404(User, username=username)
+        return render(request, 'user/profile.html', {'user': user})
+    else:
+        return HttpResponse(
+            f'Stop watching from the sidelines, register and start making'
+            f' yourself!',
+            404)
 
+
+def edit_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    if request.method == "POST":
+        user.username = request.POST.get('username')
+        profile = user.profile
+        profile.birth_date = request.POST.get('birth_date')
+        profile.country = request.POST.get('country')
+        profile.height = request.POST.get('height')
+        profile.weight = request.POST.get('weight')
+        user.save()
+        profile.save()
+    return render(request, "user/edit_profile.html", {'user': user})
+
+
+def delete_profile(request, username):
+    user = get_object_or_404(User, username=username)
+    if request.method == "POST":
+        user.delete()
+        return render(request, 'user/successfully_delete_profile')
+    return render(request, 'user/delete_profile.html', {"user":user})
